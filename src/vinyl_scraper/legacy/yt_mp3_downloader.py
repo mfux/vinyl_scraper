ebbd6de6fdc9763e082173d1afe9ba9da8c8c05f
mfux <maxfuchs@protonmail.com>
2023-01-02 19:01:38 +0100

    black formatting

--- src/vinyl_scraper/legacy/yt_mp3_downloader.py
+++ src/vinyl_scraper/legacy/yt_mp3_downloader.py
@@ -3,13 +3,15 @@ from youtube_dl import YoutubeDL
 
 def download(url, tfn):
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': tfn + '.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
+        "format": "bestaudio/best",
+        "outtmpl": tfn + ".%(ext)s",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
     }
     with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
