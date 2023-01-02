459a5acb59032bd381ebaf3e2af1339e4468d423
mfux <maxfuchs@protonmail.com>
2023-01-02 21:36:27 +0100

    bugfix

--- src/vinyl_scraper/legacy/yt_mp3_downloader.py
+++ src/vinyl_scraper/legacy/yt_mp3_downloader.py
@@ -15,10 +15,10 @@ def download(url, tfn) -> Path:
         ],
     }
     download_path = Path(str(tfn) + ".mp3")
-    if Path(download_path).exists():
-        return info, download_path
+
     with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
-        if info["duration"] <= 20 * 60:
-            ydl.download([url])
+        if download_path.exists():
+            return info, download_path
+        ydl.download([url])
     return info, download_path
