0d4da7117f5ff5e0dca6365b00a18f6b58f9a8a8
mfux <maxfuchs@protonmail.com>
2023-01-02 16:48:50 +0100

    iterate pages

--- src/vinyl_scraper/app.py
+++ src/vinyl_scraper/app.py
@@ -1,13 +1,36 @@
 # from vinyl_scraper.legacy import dbpextract
 import requests
+from bs4 import BeautifulSoup
+import re
+import io
 
 START_URL = "https://diebesondereplatte.blogspot.com/2022/12/die-besondere-platte-48-mit-simon-dorken.html"
 
 
+def next_page(text):
+    # This extract the "Older" post
+    lines = io.StringIO(text)
+    return re.findall(r"blog-pager-older-link\' href=\'(.*)\' id", lines.read())[0]
+
+
 def iterate_blog_pages(start_url):
-    r = requests.get(start_url)
-    text = r.text
-    return text
+    # load data
+    def text_soup(url):
+        r = requests.get(url)
+        text = r.text
+        soup = BeautifulSoup(text, "lxml")
+        return text, soup
+
+    # initialize loop
+    text, soup = text_soup(START_URL)
+    result = [START_URL]
+    # loop over pages
+    for i in range(10):
+        url = next_page(text)
+        text, soup = text_soup(url)
+        result.append(url)
+
+    return result
 
 
 def main():
