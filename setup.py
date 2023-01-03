5443a9b2dccb0a59d6990ce6930f78c7f4fc398e
mfux <maxfuchs@protonmail.com>
2023-01-03 01:26:21 +0100

    add setup info

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup
 
 setup(
     name="vinyl_scraper",
+    description="convert music blog into local music collection",
+    url="https://github.com/mfux/vinyl_scraper",
+    author="mfpypi",
+    author_email="mfpypi@elektronmail.de",
+    license="MIT",
     version="0.1",
+    zip_safe=False,
     packages=["vinyl_scraper"],
     package_dir={"": "src"},
     install_requires=[
@@ -15,4 +21,7 @@ setup(
         "eyed3",
         "lxml",
     ],
+    entry_points={
+        "console_scripts": ["vinyl-scrap=vinyl_scraper.app:main"],
+    },
 )
